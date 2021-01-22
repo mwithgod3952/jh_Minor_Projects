098b1598d75b7f9ae87f975c74474d1bd72d2a68
mwithgod3952 <39295904+mwithgod3952@users.noreply.github.com>
2021-01-23 00:38:53 +0900

    Update Maincocde_Basedon_Randomforest.R

--- Random_Forest_암 진단판정에_대한_확률적_접근/Maincocde_Basedon_Randomforest.R
+++ Random_Forest_암 진단판정에_대한_확률적_접근/Maincocde_Basedon_Randomforest.R
@@ -1,5 +1,8 @@
 library(mlbench)
 library(dplyr)
+library(randomForest)
+library(cluster)
+library(cluster)
 
 "Random_Forest_암 진단판정에_대한_확률적_접근"
 
@@ -18,6 +21,7 @@ distinct(select(df, Class))
 # 1    benign
 # 6 malignant
 
+#' @ST2_Test/Trainset_생성
 set.seed(123)
 train.idx = sample(nrow(df), 0.7*nrow(df))
 df.trainset = df[train.idx,]
@@ -32,5 +36,26 @@ table(df.testset$Class)
 # > table(df.testset$Class)
 # benign malignant 
 # 150        60 
+
+#' @ST4_Model-Random_Forest
+df.forest <- randomForest(Class ~ ., data = df.trainset,
+                          na.action = na.roughfix,
+                          importance = TRUE)
+
+#' @결과확인1_ 예측결과 확률확인
+df.forest.pred <- predict(df.forest, newdata = df.testset, type = "prob")
+#' @결과확인1_ 예측결과 번주확인
+df.forest.pred <- predict(df.forest, newdata = df.testset, type = "response")
+
+#' @ST5_예측정확도 확인 
+table(df.testset$Class, df.forest.pred,
+      dnn = c("Actual", "Predicted"))
+
+# Actual      benign  malignant
+# benign        142          6
+# malignant       1         58
+
+mean(df.testset$Class == df.forest.pred, na.rm = TRUE)
+#' @[1]_0.9661836, 예측정확도 96%
 
 
